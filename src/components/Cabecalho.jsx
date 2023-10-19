6f40abbaae326bf95394c6e098d8fca2edf28a46
leoleoyuuki <rm550373@fiap.com.br>
2023-10-19 09:16:27 -0300

    tailwind start

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Cabecalho() {
   return (
-    <header>
-          <nav>
+    <header className="bg-gray-800 text-white h-16">
+          <nav className='flex justify-around '>
             <Link href="/produtos/estaticas/tenis">TENIS</Link>
             <Link href={"/produtos/dinamicas/estranho/tenis-preto"}>TENIS PRETO</Link>
             <Link href="/produtos/dinamicas/vestuario/camisa-xadrez">Camisa-X</Link>
