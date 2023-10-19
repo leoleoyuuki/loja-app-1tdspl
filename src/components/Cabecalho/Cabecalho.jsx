9d246059fb26ed461e70a28c563c57331e39defc
leoleoyuuki <rm550373@fiap.com.br>
2023-10-19 10:06:55 -0300

    CABECALHO ARRUMADO E FOOTER CRIADO

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Cabecalho() {
+  return (
+    <header className="bg-gray-800 text-white ">
+          <nav className='flex justify-around items-center h-16'>
+            <Link className='linkCabecalho' href="/produtos/estaticas/tenis">TENIS</Link>
+            <Link className='linkCabecalho' href={"/produtos/dinamicas/estranho/tenis-preto"}>TENIS PRETO</Link>
+            <Link className='linkCabecalho' href="/produtos/dinamicas/vestuario/camisa-xadrez">Camisa-X</Link>
+          </nav>
+        </header>
+  )
+}
---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className=' bg-green-100 text-center fixed justify-around w-full h-15 bottom-0'>
+      <div>
+        <p>Desenvolvido por: <span className="font-bold text-blue-400">LeoYuuki</span></p>
+        <p>NEXT.js - React.js - GitHub - TailwindCss</p>
+        </div>
+
+    </footer>
+  )
+}
